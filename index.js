1d946c12308bbc7b0b16d6223969c4be03760a18
Lenny <kiddoyk@gmail.com>
2020-02-09 17:00:27 +0800

    new files

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+/*
+=================================
+Class: 1B06
+
+Admission Number: 1949913
+
+Name: Ho Yean Kit
+=================================
+ */
+
 var express = require('express');
 var app = express();
 
@@ -6,18 +16,33 @@ const servestatic = require('serve-static');
 app.use(servestatic(__dirname + "/public"));
 
 // "/" is to add in http://localhost:3001/login
-app.get("/", (req, res) => {
+
+
+// login page
+app.get("/login", (req, res) => {
     res.sendFile("/public/index.html", { root: __dirname });
 });
 
 
+// listing page
 app.get("/listings", (req, res) => {
     res.sendFile("/public/listings.html", { root: __dirname });
 });
 
-// "/" is to add in http://localhost:3001/login
-app.get("/users/", (req, res) => {
-    res.sendFile("/public/users.html", { root: __dirname });
+// sell page
+app.get("/selling", (req, res) => {
+    res.sendFile("/public/sell.html", { root: __dirname });
+});
+
+
+// search page
+app.get("/search/", (req, res) => {
+    res.sendFile("/public/search.html", { root: __dirname });
+});
+
+// profile page
+app.get("/profile/", (req, res) => {
+    res.sendFile("/public/profile.html", { root: __dirname });
 });
 
 
